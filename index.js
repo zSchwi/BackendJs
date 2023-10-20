9c26c28731087fc372df5b2bc9f6a2fa951e89c2
Rafael Furtado <rafa.fmorais2001@gmail.com>
2023-10-20 14:26:42 -0300

    Installed nodemon and configured script dev

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello world")
+const express = require('express')
+const app = express()
+const port = 3000
+/**-----Chamadas------- */
+app.get('/', function (req, res) { // / pega (get) tudo que está nessa rota, no caso, app.send(hello world)
+    res.send("Hello world")
+})
+
+app.get('/users', function (req, res){
+    res.send("Data of Users")
+})
+
+
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`)
+})
